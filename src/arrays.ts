a85b39b37f0e3d5e372a6cbfe4aa37254cef8cde
Michael Lutz <mikelutz2400@gmail.com>
2024-02-13 22:22:43 -0500

    removed unused imports

--- src/arrays.ts
+++ src/arrays.ts
@@ -1,6 +1,3 @@
-import { clear } from "console";
-import { Prev } from "react-bootstrap/esm/PageItem";
-
 /**
  * Consume an array of numbers, and return a new array containing
  * JUST the first and last number. If there are no elements, return
